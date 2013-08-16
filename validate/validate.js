328381a26da9acdf2eba75710098762a43cc8a83
Gary S <Gary7770@Hotmail.com>
2013-08-16 12:04:50 -0400

    Update validate.js
    Added in compare functionality. ie: Changing a password requires the new & confirm new passwords.
    Up next, need to validate password complexity & test that this rookie didn't break any existing functionality.

--- validate/validate.js
+++ validate/validate.js
@@ -1,91 +1,99 @@
-var _ = require((typeof ENV_TEST === 'boolean') ? 'alloy' : 'underscore')._;
+var _ = require(( typeof ENV_TEST === 'boolean') ? 'alloy' : 'underscore')._;
 
 function validate(value, options) {
     options = _.defaults(options || {}, {
-       trim: true,
-       required: true,
-       label: 'Value'
+        trim : true,
+        required : true,
+        label : 'Value',
+        compare : true
     });
-    
     if (_.isObject(value)) {
-        
-        if (typeof value.value === 'string') {
-            
+
+        if ( typeof value.value === 'string') {
+
             if (value.hintText) {
                 options.label = value.hintText;
             }
-            
+
             value = value.value;
-            
-        } else {
+
+        } else if (options['compare'] !== true) {
             var values = {};
 
-            _.each(_.keys(value), function (key) {
+            _.each(_.keys(value), function(key) {
                 var keyOptions, keyValue;
-                
+
                 if (_.isArray(value[key])) {
                     keyValue = value[key][0];
                     keyOptions = _.defaults(value[key][1] || {}, options);
-                    
+
                 } else {
                     keyValue = value[key];
                     keyOptions = _.clone(options);
                 }
-                
+
                 values[key] = validate(keyValue, keyOptions);
-                
+
                 return;
             });
-            
+
             return values;
         }
     }
-   
-    if (typeof value === 'string' && options.trim) {
+
+    if ( typeof value === 'string' && options.trim) {
         value = value.trim();
     }
-    
     delete options.trim;
 
     if (options.required && _.isEmpty(value)) {
         throw String.format(L('validate_required', '%s is required.'), options.label);
     }
-    
     delete options.required;
-    
+
     var error;
-    
-    _.each(options, function (setting, key) {
-        
+
+    _.each(options, function(setting, key) {
+
         if (setting === false) {
             return;
         }
 
         switch (key) {
-            
+
             case 'email':
                 var email = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
-                if (!email.test(value)) throw String.format(L('validate_required', '%s is no email address.'), options.label);
+                if (!email.test(value))
+                    throw String.format(L('validate_required', '%s is no email address.'), options.label);
                 break;
-                
+
             case 'regexp':
-                if (!setting.test(value)) throw String.format(L('validate_regexp', '%s is not valid.'), options.label);
+                if (!setting.test(value))
+                    throw String.format(L('validate_regexp', '%s is not valid.'), options.label);
                 break;
-                
+
             case 'numeric':
-                if (!_.isFinite(value)) throw String.format(L('validate_numeric', '%s is not a number.'), options.label);
+                if (!_.isFinite(value))
+                    throw String.format(L('validate_numeric', '%s is not a number.'), options.label);
+                break;
+
+            case "compare":
+                for (var i = 1, j = value.compare.length; j > i; i++) {
+                    if (value.compare[i - 1] !== value.compare[i])
+                        throw L("validate_compare", "The values are not equal");
+                }
                 break;
         }
-       
+
         return;
     });
-        
+
     return value;
 }
 
-if (typeof exports !== 'undefined') {
-        if (typeof module !== 'undefined' && module.exports) {
-                exports = module.exports = validate;
-        }
-        exports.validate = validate;
+if ( typeof exports !== 'undefined') {
+    if ( typeof module !== 'undefined' && module.exports) {
+        exports = module.exports = validate;
+    }
+    exports.validate = validate;
 }
